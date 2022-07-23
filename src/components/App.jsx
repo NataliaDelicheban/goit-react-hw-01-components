74cf0061cb24964a603cec2558db3c33006a7423
NataliaDelicheban <nataliamansarliyska@gmail.com>
2022-07-23 21:14:05 +0200

    create task 2

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,6 +1,8 @@
 import { Profile } from './Profile/Profile';
+import { Statistics } from './Statistics/Statistics';
 
-import user from '../data/user.json'
+import user from '../data/user.json';
+import data from '../data/data.json';
 
 export const App = () => {
   return (
@@ -11,6 +13,7 @@ export const App = () => {
         location={user.location}
         avatar={user.avatar}
         stats={user.stats} />
+      <Statistics title="UPLOAD STATS" stats={data} />
     </div>
   );
 };
